5940b05981dd1944ecd790cf4e0c46674612b219
hoovard <hoovard@hoovard-ThinkPad-L512.(none)>
2012-03-11 12:37:56 +0000

    hco.pe web 11032012-12-37

---
+++ img/index.php
@@ -0,0 +1,1 @@
+<?php Header( "Location: http://hco.pe" ); ?>
--- index.php
+++ index.php
@@ -4,42 +4,45 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
     <head>
-        <title>hco.pe | home</title>
+        <title>hco.pe | idx</title>
         <style type="text/css">
-            
-             body
-             {
-                 font-family:courier;
-                 font-size: 100%;
-             }
-             
-             div#main {width:100%; border:0px}
-             div#wrapper 
-             {
-                width:421px;
-                margin-left:auto; 
-                margin-right:auto; 
-                border:0px solid #000;
-                text-align:center;
-                margin-top: 40px;
-             }
-             div#idximage 
-             {
-                width:421px; 
-                height:542px; 
-                border:0px;
-                background-image:url('img/idx.png');
-             }
-             span.idxtxt {color:#ca9c4d;}
-            
+                
+	    body
+	    {
+		    font-family:courier;
+		    font-size: 100%;
+		    background-color:#fff;
+	    }
+
+	    div#main {width:100%; border:0px}
+	    div#wrapper 
+	    {
+		    width:227px;
+		    margin-left:auto; 
+		    margin-right:auto; 
+		    border:0px;
+		    text-align:center;
+		    margin-top: 40px;
+	    }
+	    div#idximage 
+	    {
+		    width:227px; 
+		    height:292px; 
+		    border:0px;
+		    background-image:url('img/idx.png');
+	    }
+	    span.idxtxt {color:#ca9c4d;}
+                
         </style>
     </head>
-    <body>
+<body>
         <div id="main">
             <div id="wrapper">
-                <h3><img alt="hco.pe" src="graphic_header.php?t=hco.pe&h=3&bg=ff|ff|ff&fc=ca|9c|4d"  border="0" /></h3>
-                <h3><a href="http://cv.hco.pe"><img alt="cv" src="graphic_header.php?t=- C V -&h=3&bg=ff|ff|ff&fc=ca|9c|4d" border="0" /></a></h3>
-                <div id="idximage"><img src="img/pix.gif" width="421" height="542" /></div>
+                <h3><img alt="hco.pe" src="graphic_header.php?t=hco.pe&h=3&bg=ff|ff|ff&fc=99|00|00"  border="0" /></h3>
+                <h3><a target="aBlogWindow" href="http://final-days-of-salt.blogspot.com"><img alt="art blog" src="graphic_header.php?t=Art Blog&h=3&bg=ff|ff|ff&fc=ca|9c|4d" border="0" /></a></h3>
+                <h3><a target="pBlogWindow" href="http://saline-well.blogspot.com/"><img alt="photo blog" src="graphic_header.php?t=Photo Blog&h=3&bg=ff|ff|ff&fc=ca|9c|4d" border="0" /></a></h3>
+                <h3><a target="daWindow" href="http://final-days-of-salt.deviantart.com/gallery/"><img alt="deviant art" src="graphic_header.php?t=Deviant Art&h=3&bg=ff|ff|ff&fc=ca|9c|4d" border="0" /></a></h3>
+                <div id="idximage"><img src="img/pix.gif" width="227" height="292" /></div>
             </div>
         </div>
     </body>
